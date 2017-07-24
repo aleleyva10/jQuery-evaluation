618ee392b7458f8c23920a7acf9c9424c777acda
Alessandro Leyva <josealeyva87@gmail.com>
2017-07-24 13:37:09 -0500

    Some changes

--- scripts.js
+++ scripts.js
@@ -6,38 +6,50 @@ function randomNumber(min, max) {
   return Math.floor(Math.random() * (1 + max - min) + min);
 }
 
-function rgbValue() {
-  var r = randomNumber(0, 255);
-  var g = randomNumber(0, 255);
-  var b = randomNumber(0, 255);
-
-  return 'rgb(' + r + ', ' + g + ', ' + b + ')';
-}
-
+var redCount = 0;
+var blueCount = 0;
+var greenCount = 0;
+var yellowCount = 0;
 
 var addBlock = function() {
-  var $block = ('<div class="block" style="background-color:' + rgbValue() + '"></div>');
-  $('main').append($block);
-  updateCounters();
+  var color = randomNumber(0, 3);
+
+  if (color === 0) {
+    color = "red";
+    redCount++;
+    $('#red').text(redCount);
+    console.log('You created a red block');
+  } else if (color === 1) {
+    color = "blue";
+    blueCount++;
+    $('#blue').text(blueCount);
+    console.log('You created a blue block');
+  } else if (color === 2) {
+    color = "green";
+    greenCount++;
+    $('#green').text(greenCount);
+    console.log('You created a green block');
+  } else if (color === 3) {
+    color = "yellow";
+    yellowCount++;
+    $('#yellow').text(yellowCount);
+    console.log('You created a yellow block');
+  }
+
+  $('main').append("<div class='color-cube " + color + "'></div>");
 };
 
 var deleteBlock = function() {
   console.log('deleted block');
   $(this).remove();
-  updateCounters();
+  redCount++;
+  blueCount++;
+  greenCount--;
+  yellowCount--;
 };
 
-
-var updateCounters = function() {
-  console.log('in updateCounters');
-
-  $('#red').text($('.blockButton').length);
-  $('#blue').text($('.blockButton').length);
-  $('#yellow').text($('.blockButton').length);
-  $('#green').text($('.blockButton').length);
-};
 
 function onReady() {
   $('.blockButton').on("click", addBlock);
-  $('main').on('click', '.block', deleteBlock);
+  $('main').on('click', '.color-cube', deleteBlock);
 }
